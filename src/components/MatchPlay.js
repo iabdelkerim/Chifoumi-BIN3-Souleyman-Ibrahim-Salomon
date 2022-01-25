cbe04e5cb3d5483a7078b9059627ab2dd3d30c74
SouleyDia <soul@naker.io>
2022-01-25 19:26:39 +0100

    re-structure

--- src/components/MatchPlay.js
+++ src/components/MatchPlay.js
@@ -1,71 +1,70 @@
 import React from 'react';
-import { Button, Flex } from '@adobe/react-spectrum';
+import { Button, Flex, Heading } from '@adobe/react-spectrum';
 import { useEffect, useState } from 'react';
 
 function MatchPlay() {
     const [play, setPlay] = useState();
+
     useEffect(() => {
-        fetch('http://fauques.freeboxos.fr:3000/matches/:id', {
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('token')
+        fetch(
+            'http://fauques.freeboxos.fr:3000/matches/?id=61eafe2a9776211826834e72',
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('token')
+                }
             }
-        })
+        )
             .then(res => res.json())
             .then(data => setPlay(data));
-    }, []);
+    }, [play]);
 
     return (
         <>
             <Flex
-                direction="row" 
+                direction="row"
                 alignItems="center"
                 justifyContent="center"
                 alignContent="center"
-                 
                 gap="size-100"
             >
-                <p><h1>Partie de Chifoumi</h1></p>
+                <Heading level={1}>Partie de Chifoumi</Heading>
             </Flex>
-            
+
             <Flex
-                direction="row" 
+                direction="row"
                 alignItems="center"
                 justifyContent="center"
                 alignContent="center"
-                height="size-800" 
+                height="size-800"
                 gap="size-100"
             >
-                <Button width="size-800" >Rock</Button>
-                <Button width="size-800" >Paper</Button>
-                <Button width="size-1200" >Scissors</Button>
+                <Button width="size-800">Rock</Button>
+                <Button width="size-800">Paper</Button>
+                <Button width="size-1200">Scissors</Button>
             </Flex>
-            
+
             {play?.map(player => (
                 <Flex
-                    direction="row" 
+                    direction="row"
                     alignItems="center"
                     justifyContent="center"
                     alignContent="center"
-                    
                     gap="size-100"
                 >
-                    <p><h5> user :  {player.user1} - 0</h5></p>
+                    <Heading level={3}> user : {player.user1} - 0</Heading>
                 </Flex>
-                
             ))}
-            
-            
+
             <Flex
-                direction="row" 
+                direction="row"
                 alignItems="center"
                 justifyContent="center"
                 alignContent="center"
-                 
                 gap="size-100"
             >
-                <p><h5> Score :  0 - 0</h5></p>
+                <Heading level={4}> Score : 0 - 0</Heading>
             </Flex>
-            
         </>
     );
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import MatchList from './MatchList';
+import MatchPlay from './MatchPlay';
+
+export { MatchList, MatchPlay };
