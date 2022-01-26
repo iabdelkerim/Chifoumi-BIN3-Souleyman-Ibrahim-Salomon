3b769b9f23cec157501c35a0604c339b718e9ca3
SouleyDia <soul@naker.io>
2022-01-26 14:41:29 +0100

    Reworked cards and player display

--- src/components/MatchPlay.js
+++ src/components/MatchPlay.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Button, Flex, Heading, View } from '@adobe/react-spectrum';
+import {
+    Text,
+    Item,
+    Flex,
+    ActionGroup,
+    Heading,
+    View
+} from '@adobe/react-spectrum';
 import { useEffect, useState } from 'react';
 import Player from './player';
+import Turn from './turn';
+import Hand0 from '@spectrum-icons/workflow/Hand0';
+import Hand from '@spectrum-icons/workflow/Hand';
+import Hand2 from '@spectrum-icons/workflow/Hand2';
 
 function MatchPlay(id) {
-    const [play, setPlay] = useState([]);
+    const [play, setPlay] = useState();
 
     useEffect(() => {
         fetch(`http://fauques.freeboxos.fr:3000/matches/?id='${id}`, {
@@ -22,8 +33,8 @@ function MatchPlay(id) {
     function PlayertList() {
         return (
             <>
-                {play?.map(player => (
-                    <Player player={player} />
+                {play?.map(user => (
+                    <Player player={user} />
                 ))}
             </>
         );
@@ -49,9 +60,25 @@ function MatchPlay(id) {
                 height="size-800"
                 gap="size-100"
             >
-                <Button width="size-800">Rock</Button>
-                <Button width="size-800">Paper</Button>
-                <Button width="size-1200">Scissors</Button>
+                <ActionGroup
+                    onAction={move => {
+                        Turn(move);
+                    }}
+                    marginEnd="size-130"
+                >
+                    <Item key="rock">
+                        <Hand0 size="XXL" />
+                        <Text>Rock</Text>
+                    </Item>
+                    <Item key="paper">
+                        <Hand size="XXL" />
+                        <Text>Paper</Text>
+                    </Item>
+                    <Item key="scissors">
+                        <Hand2 size="XXL" />
+                        <Text>Scissors</Text>
+                    </Item>
+                </ActionGroup>
             </Flex>
             <PlayertList />
             <Flex
